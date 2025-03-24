63427e16351f824d211b2634482ef59a0737250e
albert2910 <tplinh29102000@gmail.com>
2025-03-24 22:48:41 +0700

    add post repository impl

--- src/main/java/org/example/repository/PostRepository.java
+++ src/main/java/org/example/repository/PostRepository.java
@@ -1,4 +1,13 @@
 package org.example.repository;
 
+import org.example.entities.Post;
+
+import java.util.List;
+
 public interface PostRepository {
+    List<Post> findAll();
+    Post findById(Long id);
+    Post save(Post post);
+    void delete(Post post);
+    Post update(Post post);
 }
---
+++ src/main/java/org/example/repository/impl/PostRepositoryImpl.java
@@ -0,0 +1,7 @@
+package org.example.repository.impl;
+
+import org.example.repository.PostRepository;
+
+public class PostRepositoryImpl implements PostRepository {
+
+}
